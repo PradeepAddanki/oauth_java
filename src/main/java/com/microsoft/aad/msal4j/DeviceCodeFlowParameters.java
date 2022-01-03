5fa029f9b8c1558a8efe05844ceac3337eb83aa6
pradeep <kumarap29@gmail.com>
2022-01-03 17:32:25 +0530

    Update

--- src/main/java/com/microsoft/aad/msal4j/DeviceCodeFlowParameters.java
+++ src/main/java/com/microsoft/aad/msal4j/DeviceCodeFlowParameters.java
@@ -1,58 +1,94 @@
 package com.microsoft.aad.msal4j;
 
-import lombok.*;
-import lombok.experimental.Accessors;
-
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
 
-import static com.microsoft.aad.msal4j.ParameterValidationUtils.validateNotNull;
-
-/**
- * Object containing parameters for device code flow. Can be used as parameter to
- * {@link PublicClientApplication#acquireToken(DeviceCodeFlowParameters)}. For more details,
- * see https://aka.ms/msal4j-device-code
- */
-@Builder
-@Accessors(fluent = true)
-@Getter
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class DeviceCodeFlowParameters implements IAcquireTokenParameters {
-
-
     private Set<String> scopes;
-
     private Consumer<DeviceCode> deviceCodeConsumer;
-
-    /**
-     * Claims to be requested through the OIDC claims request parameter, allowing requests for standard and custom claims
-     */
     private ClaimsRequest claims;
-
-    /**
-     * Adds additional headers to the token request
-     */
     private Map<String, String> extraHttpHeaders;
-
-    /**
-     * Overrides the tenant value in the authority URL for this request
-     */
     private String tenant;
 
-    private static DeviceCodeFlowParametersBuilder builder() {
-
-        return new DeviceCodeFlowParametersBuilder();
+    private static DeviceCodeFlowParameters.DeviceCodeFlowParametersBuilder builder() {
+        return new DeviceCodeFlowParameters.DeviceCodeFlowParametersBuilder();
     }
 
+    public static DeviceCodeFlowParameters.DeviceCodeFlowParametersBuilder builder(Set<String> scopes, Consumer<DeviceCode> deviceCodeConsumer) {
+        ParameterValidationUtils.validateNotNull("scopes", scopes);
+        return builder().scopes(scopes).deviceCodeConsumer(deviceCodeConsumer);
+    }
 
-    public static DeviceCodeFlowParametersBuilder builder
-    (Set<String> scopes, Consumer<DeviceCode> deviceCodeConsumer) {
+    public Set<String> scopes() {
+        return this.scopes;
+    }
 
-        validateNotNull("scopes", scopes);
+    public Consumer<DeviceCode> deviceCodeConsumer() {
+        return this.deviceCodeConsumer;
+    }
 
-        return builder()
-                .scopes(scopes)
-                .deviceCodeConsumer(deviceCodeConsumer);
+    public ClaimsRequest claims() {
+        return this.claims;
+    }
+
+    public Map<String, String> extraHttpHeaders() {
+        return this.extraHttpHeaders;
+    }
+
+    public String tenant() {
+        return this.tenant;
+    }
+
+    private DeviceCodeFlowParameters(Set<String> scopes, Consumer<DeviceCode> deviceCodeConsumer, ClaimsRequest claims, Map<String, String> extraHttpHeaders, String tenant) {
+        this.scopes = scopes;
+        this.deviceCodeConsumer = deviceCodeConsumer;
+        this.claims = claims;
+        this.extraHttpHeaders = extraHttpHeaders;
+        this.tenant = tenant;
+    }
+
+    public static class DeviceCodeFlowParametersBuilder {
+        private Set<String> scopes;
+        private Consumer<DeviceCode> deviceCodeConsumer;
+        private ClaimsRequest claims;
+        private Map<String, String> extraHttpHeaders;
+        private String tenant;
+
+        DeviceCodeFlowParametersBuilder() {
+        }
+
+        public DeviceCodeFlowParameters.DeviceCodeFlowParametersBuilder scopes(Set<String> scopes) {
+            this.scopes = scopes;
+            return this;
+        }
+
+        public DeviceCodeFlowParameters.DeviceCodeFlowParametersBuilder deviceCodeConsumer(Consumer<DeviceCode> deviceCodeConsumer) {
+            this.deviceCodeConsumer = deviceCodeConsumer;
+            return this;
+        }
+
+        public DeviceCodeFlowParameters.DeviceCodeFlowParametersBuilder claims(ClaimsRequest claims) {
+            this.claims = claims;
+            return this;
+        }
+
+        public DeviceCodeFlowParameters.DeviceCodeFlowParametersBuilder extraHttpHeaders(Map<String, String> extraHttpHeaders) {
+            this.extraHttpHeaders = extraHttpHeaders;
+            return this;
+        }
+
+        public DeviceCodeFlowParameters.DeviceCodeFlowParametersBuilder tenant(String tenant) {
+            this.tenant = tenant;
+            return this;
+        }
+
+        public DeviceCodeFlowParameters build() {
+            return new DeviceCodeFlowParameters(this.scopes, this.deviceCodeConsumer, this.claims, this.extraHttpHeaders, this.tenant);
+        }
+
+        public String toString() {
+            return "DeviceCodeFlowParameters.DeviceCodeFlowParametersBuilder(scopes=" + this.scopes + ", deviceCodeConsumer=" + this.deviceCodeConsumer + ", claims=" + this.claims + ", extraHttpHeaders=" + this.extraHttpHeaders + ", tenant=" + this.tenant + ")";
+        }
     }
 }
